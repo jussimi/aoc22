48219bb542c73464f5685b183ec23751993416f0
Jussi Mäki-Ikola <jussimakiikola@gmail.com>
2022-12-06 13:07:53 +0200

    Completed day1

--- day1/main.go
+++ day1/main.go
@@ -3,16 +3,57 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 //go:embed data.txt
 var data []byte
 
 func main() {
-	answer := run(data)
-	fmt.Println("answer", answer)
+	fmt.Println("answer1", runPart1(data))
+	fmt.Println("answer2", runPart2(data))
 }
 
-func run(input []byte) int64 {
-	return 24000
+func findElves(input []byte) [][]string {
+	str := strings.Replace(string(input), "\r", "", -1)
+	split := strings.Split(str, "\n\n")
+	elves := [][]string{}
+	for _, elfLines := range split {
+		elves = append(elves, strings.Split(elfLines, "\n"))
+	}
+	return elves
+}
+
+func findElvesSumN(input []byte, n int) int {
+	elves := findElves(input)
+
+	sums := []int{}
+	for _, elf := range elves {
+		sum := 0
+		for _, line := range elf {
+			num, _ := strconv.Atoi(line)
+			sum += num
+		}
+		sums = append(sums, sum)
+	}
+
+	sort.Slice(sums, func(i, j int) bool {
+		return sums[i] > sums[j]
+	})
+
+	sumOfMaxN := 0
+	for _, s := range sums[0:n] {
+		sumOfMaxN += s
+	}
+	return sumOfMaxN
+}
+
+func runPart1(input []byte) int {
+	return findElvesSumN(input, 1)
+}
+
+func runPart2(input []byte) int {
+	return findElvesSumN(input, 3)
 }
