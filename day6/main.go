058593a49f97c0120c1fd9d835cfaf583f47bc3d
Jussi Mäki-Ikola <jussimakiikola@gmail.com>
2022-12-07 23:07:23 +0200

    Completed day6

--- day6/main.go
+++ day6/main.go
@@ -9,10 +9,42 @@ import (
 var data []byte
 
 func main() {
-	answer := run(data)
-	fmt.Println("answer", answer)
+	fmt.Println("answer1", runPart1(data))
+	fmt.Println("answer2", runPart2(data))
 }
 
-func run(input []byte) int64 {
-	return 24000
+func areUnique(bytes []byte) bool {
+	byteMap := make(map[byte]bool)
+	for _, b := range bytes {
+		if exists := byteMap[b]; exists {
+			return false
+		}
+		byteMap[b] = true
+	}
+	return true
+}
+
+func findFirstUniqueBytes(input []byte, amount int) int {
+	queue := []byte{}
+	for i, b := range input {
+		queue = append(queue, b)
+
+		if len(queue) < amount+1 {
+			continue
+		}
+		queue = queue[1:]
+
+		if areUnique(queue) {
+			return i + 1
+		}
+	}
+	panic("not found")
+}
+
+func runPart1(input []byte) int {
+	return findFirstUniqueBytes(input, 4)
+}
+
+func runPart2(input []byte) int {
+	return findFirstUniqueBytes(input, 14)
 }
--- day6/main_test.go
+++ day6/main_test.go
@@ -3,17 +3,37 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"strings"
 	"testing"
 )
 
 //go:embed test.txt
 var test []byte
 
-func TestModule(t *testing.T) {
-	const expected = 24000 // REPLACE ME
-	fmt.Println(test)
-	ans := run(test)
-	if ans != expected {
-		t.Errorf("Received: %d; Expected %d", ans, expected)
+func TestPart1(t *testing.T) {
+	var expected = []int{7, 5, 6, 10, 11}
+
+	for i, line := range strings.Split(string(test), "\n") {
+		testname := fmt.Sprintf("Line %d", i)
+		t.Run(testname, func(t *testing.T) {
+			ans := runPart1([]byte(line))
+			if ans != expected[i] {
+				t.Errorf("Received: %d; Expected %d", ans, expected[i])
+			}
+		})
+	}
+}
+
+func TestPart2(t *testing.T) {
+	var expected = []int{19, 23, 23, 29, 26}
+
+	for i, line := range strings.Split(string(test), "\n") {
+		testname := fmt.Sprintf("Line %d", i)
+		t.Run(testname, func(t *testing.T) {
+			ans := runPart2([]byte(line))
+			if ans != expected[i] {
+				t.Errorf("Received: %d; Expected %d", ans, expected[i])
+			}
+		})
 	}
 }
