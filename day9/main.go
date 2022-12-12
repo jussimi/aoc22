85fae4d019fefb29c5486db4cd939f2b5d65693a
Jussi Mäki-Ikola <jussimakiikola@gmail.com>
2022-12-12 20:54:14 +0200

    Completed day9

--- day9/main.go
+++ day9/main.go
@@ -3,16 +3,96 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 //go:embed data.txt
 var data []byte
 
 func main() {
-	answer := run(data)
-	fmt.Println("answer", answer)
+	fmt.Println("answer1", runPart1(data))
+	fmt.Println("answer2", runPart2(data))
 }
 
-func run(input []byte) int64 {
-	return 24000
+var directions = map[string][2]int{
+	"R": {0, 1},
+	"L": {0, -1},
+	"U": {1, 0},
+	"D": {-1, 0},
+}
+
+func id(point [2]int) string {
+	return fmt.Sprintf("%d-%d", point[0], point[1])
+}
+
+func sign(n int) int {
+	if n < 0 {
+		return -1
+	}
+	return 1
+}
+
+func abs(n int) int {
+	return sign(n) * n
+}
+
+func moveRope(input []byte, knotCount int) int {
+
+	knots := make([][2]int, knotCount)
+	for i := range knots {
+		knots[i] = [2]int{0, 0}
+	}
+
+	visited := make(map[string]bool)
+	visited[id(knots[0])] = true
+
+	for _, instruction := range strings.Split(string(input), "\n") {
+		split := strings.Fields(instruction)
+		direction := directions[split[0]]
+		amount, _ := strconv.Atoi(split[1])
+
+		for i := 0; i < amount; i += 1 {
+			head := knots[0]
+			knots[0] = [2]int{
+				head[0] + direction[0],
+				head[1] + direction[1],
+			}
+
+			for j := 1; j < knotCount; j += 1 {
+				current := knots[j]
+				previous := knots[j-1]
+				dx := previous[0] - current[0]
+				dy := previous[1] - current[1]
+
+				if abs(dx) == 2 {
+					current[0] = current[0] + sign(dx)
+					if abs(dy) == 1 {
+						current[1] = previous[1]
+					}
+				}
+
+				if abs(dy) == 2 {
+					current[1] = current[1] + sign(dy)
+					if abs(dx) == 1 {
+						current[0] = previous[0]
+					}
+				}
+				knots[j] = current
+			}
+
+			tail := knots[len(knots)-1]
+			visited[id(tail)] = true
+		}
+	}
+
+	return len(visited)
+}
+
+func runPart1(input []byte) int {
+	return moveRope(input, 2)
+}
+
+func runPart2(input []byte) int {
+	return moveRope(input, 10)
 }
--- day9/main_test.go
+++ day9/main_test.go
@@ -3,17 +3,32 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"strings"
 	"testing"
 )
 
 //go:embed test.txt
 var test []byte
 
-func TestModule(t *testing.T) {
-	const expected = 24000 // REPLACE ME
-	fmt.Println(test)
-	ans := run(test)
+func TestPart1(t *testing.T) {
+	const expected = 13
+	input := strings.Split(string(test), "\n\n")[0]
+	ans := runPart1([]byte(input))
 	if ans != expected {
 		t.Errorf("Received: %d; Expected %d", ans, expected)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	var expected = []int{1, 36}
+
+	for i, test := range strings.Split(string(test), "\n\n") {
+		testname := fmt.Sprintf("Test %d", i)
+		t.Run(testname, func(t *testing.T) {
+			ans := runPart2([]byte(test))
+			if ans != expected[i] {
+				t.Errorf("Received: %d; Expected %d", ans, expected[i])
+			}
+		})
 	}
 }
