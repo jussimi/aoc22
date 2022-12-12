643bc564d835b9c5fab6103490a5628e78c27e76
Jussi Mäki-Ikola <jussimakiikola@gmail.com>
2022-12-12 22:04:44 +0200

    Completed day10

--- day10/main.go
+++ day10/main.go
@@ -3,16 +3,74 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 //go:embed data.txt
 var data []byte
 
 func main() {
-	answer := run(data)
-	fmt.Println("answer", answer)
+	fmt.Println("answer1", runPart1(data))
+	fmt.Println("answer2")
+	fmt.Println(runPart2(data))
 }
 
-func run(input []byte) int64 {
-	return 24000
+func createHistory(input []byte) []int {
+	x := 1
+
+	history := []int{x}
+
+	for _, instruction := range strings.Split(string(input), "\n") {
+		split := strings.Fields(instruction)
+
+		if split[0] == "addx" {
+			amount, _ := strconv.Atoi(split[1])
+
+			for _, val := range []int{0, amount} {
+				x += val
+				history = append(history, x)
+			}
+		} else {
+			history = append(history, x)
+		}
+	}
+	return history
+}
+
+func runPart1(input []byte) int {
+	history := createHistory(input)
+	indices := []int{20, 60, 100, 140, 180, 220}
+
+	s := 0
+	for _, i := range indices {
+		s += history[i-1] * i
+	}
+
+	return s
+}
+
+func runPart2(input []byte) string {
+	lines := []string{}
+
+	history := createHistory(input)
+
+	for i := 0; i < 6; i += 1 {
+		line := make([]string, 40)
+		for j := range line {
+			line[j] = "."
+		}
+
+		for j := range line {
+			x := history[0]
+			history = history[1:]
+
+			if j == x || j == x-1 || j == x+1 {
+				line[j] = "#"
+			}
+		}
+		lines = append(lines, strings.Join(line, ""))
+	}
+
+	return strings.Join(lines, "\n")
 }
--- day10/main_test.go
+++ day10/main_test.go
@@ -2,18 +2,35 @@ package main
 
 import (
 	_ "embed"
-	"fmt"
+	"strings"
 	"testing"
 )
 
 //go:embed test.txt
 var test []byte
 
-func TestModule(t *testing.T) {
-	const expected = 24000 // REPLACE ME
-	fmt.Println(test)
-	ans := run(test)
+func TestPart1(t *testing.T) {
+	const expected = 13140
+	ans := runPart1(test)
 	if ans != expected {
 		t.Errorf("Received: %d; Expected %d", ans, expected)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	lines := []string{
+		"##..##..##..##..##..##..##..##..##..##..",
+		"###...###...###...###...###...###...###.",
+		"####....####....####....####....####....",
+		"#####.....#####.....#####.....#####.....",
+		"######......######......######......####",
+		"#######.......#######.......#######.....",
+	}
+
+	expected := strings.Join(lines, "\n")
+
+	ans := runPart2(test)
+	if ans != expected {
+		t.Errorf("\nReceived: \n%s\nExpected \n%s", ans, expected)
 	}
 }
